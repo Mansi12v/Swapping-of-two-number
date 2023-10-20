189974e1dac05f1164a3b75effe1f9e64b84c716
Mansi12v <148568768+Mansi12v@users.noreply.github.com>
2023-10-20 21:43:27 +0530

    Add files via upload

---
+++ swapping.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class swapping {
+    public static void swap(int a , int b){
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a =" + a);
+        System.out.println("b =" + b);
+
+    }
+    public static void main(String args[]){
+        int a = 19; 
+        int b = 20;
+        swap(a, b);
+    } 
+}      
+
